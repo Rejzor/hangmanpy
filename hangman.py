18ba9c4078bc54557a71dd55969a5827dcc57c1b
Rejzor <209456@student.pwr.edu.pl>
2017-02-06 03:11:31 +0100

    Works but not punish for bad answers. It works... almost :)

--- hangman.py
+++ hangman.py
@@ -3,11 +3,6 @@ def read_data():
 	# key_word = input("Provide key word: ") #visible input data
 	key_word = getpass.getpass('Provide key word: ') #getpass module for hide input data
 	return list(key_word) #return list of key word character for example word TEST ['T','E','S','T']
-def guess_word():
-	guess_char = input("Provide character which key word contain: ")
-	return guess_char
-def check_answer(read_data,guess_word):
-	return answer_characters
 def check_char(key_word):
 	bad_answers=0
 	answer_characters = ['_' for i in range(len(key_word))]
@@ -18,9 +13,6 @@ def check_char(key_word):
 				answer_characters.pop(count)
 				answer_characters.insert(count,guess_char)
 		print(answer_characters)
-			# else:
-			# 	bad_answers+=1
-
 					
 
 check_char(read_data())
